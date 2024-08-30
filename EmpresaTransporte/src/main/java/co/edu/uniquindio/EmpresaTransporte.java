ebac80b79fc0b985432cd6b696f0c43adeac4c0a
GUCCIMADRASO <daniestiv89@gmail.com>
2024-08-29 21:57:34 -0500

    Correccion de la estructura del repositorio

---
+++ EmpresaTransporte/src/main/java/co/edu/uniquindio/EmpresaTransporte.java
@@ -0,0 +1,26 @@
+package co.edu.uniquindio;
+
+import java.util.LinkedList;
+
+public class EmpresaTransporte {
+    private LinkedList<Propietario> propietarios = new LinkedList<>();
+    private LinkedList<Usuario> usuarios = new LinkedList<>();
+    private LinkedList<VehiculoTransporte> vehiculosTransporte = new LinkedList<>();
+    private LinkedList<VehiculoCarga> vehiculosCarga = new LinkedList<>();
+
+    public LinkedList<Propietario> getPropietarios() {
+        return propietarios;
+    }
+
+    public LinkedList<Usuario> getUsuarios() {
+        return usuarios;
+    }
+
+    public LinkedList<VehiculoTransporte> getVehiculosTransporte() {
+        return vehiculosTransporte;
+    }
+
+    public LinkedList<VehiculoCarga> getVehiculosCarga() {
+        return vehiculosCarga;
+    }
+}
